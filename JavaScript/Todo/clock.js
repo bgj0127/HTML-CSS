1bc3fb82d42c86c4688fc4b11aca670cee5158fb
Bae gil jun <51225580+bgj0127@users.noreply.github.com>
2020-05-31 19:30:53 +0900

    javascript_file

--- JavaScript/Todo/clock.js
+++ JavaScript/Todo/clock.js
@@ -9,7 +9,7 @@ window.onload = function () {
     const seconds = date.getSeconds();
     clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
       minutes < 10 ? `0${minutes}` : minutes
-    }:${seconds < 10 ? `0${seconds}` : seconds}`;
+    }`;
   }
 
   function init() {
